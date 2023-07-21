999d022535812f6a75a625bdc81df8c183f6bb5f
Redexes-web <redexes75@hotmail.fr>
2023-07-21 10:34:52 +0200

    add change

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,47 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import { useEffect, useState } from 'react';
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
 }
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      {/* Keep the existing code here */}
 
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
+export default function Home({ allPostsData }) {
+	const [dynamicName, setDynamicName] = useState('[Your Name]');
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setDynamicName('New Dynamic Name');
+		}, 2000);
+		return () => clearTimeout(timeout);
+	}, []);
+	return (
+		<Layout home>
+			{/* Keep the existing code here */}
+			{/* Add this <section> tag below the existing <section> tag */}
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+							<br />
+							{dynamicName}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
